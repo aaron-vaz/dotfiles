ae8618d9d8cf275f0f914c725414f0185bb8f501
Lewis Juggins <ldjuggins@gmail.com>
2016-02-15 20:23:55 +0000

    Final fzf fix, add maglev to tmux

--- fzf/install.sh
+++ fzf/install.sh
@@ -1,6 +1,6 @@
 if test "$(which fzf)"
 then
-  cd ~/.fzf && git pull && ./install
+  cd ~/.fzf && git pull && ./install --all --no-update-rc
 else
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --all --no-update-rc
