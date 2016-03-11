5e4f7bf457b3152f215c606dd8afdc9848bee9d2
Lewis Juggins <ldjuggins@gmail.com>
2016-03-11 17:05:20 +0000

    Fix ls colours

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,7 @@
 alias reload!='. ~/.zshrc'
-[[ $(whence gls 2> /dev/null) ]] && alias ls='gls --color=auto'
+if [[ $(whence gls 2> /dev/null) ]]
+then
+    alias ls='gls --color=auto'
+else
+    alias ls='ls --color=auto'
+fi
