de5d776d613187cc2be8a46bc3dbcce4ed2c7dd2
Aaron Vaz <vazaaron08@gmail.com>
2016-08-21 11:01:28 +0100

    adding livestreamer as a pip package & fixing pip installation

--- pip/install.sh
+++ pip/install.sh
@@ -5,4 +5,4 @@ then
     curl -fsSL https://bootstrap.pypa.io/get-pip.py > /tmp/get-pip.py && python3 /tmp/get-pip.py --user && rm /tmp/get-pip.py && $PYTHONUSERBASE/bin/pip install --user --upgrade -r ~/.dotfiles/pip/requirements.txt
 fi
 
-$PYTHONUSERBASE/bin/pip install --user --upgrade -r ~/.dotfiles/pip/requirements.txt
+$PYTHONUSERBASE/bin/pip install --user --upgrade -r $DOTFILES/pip/requirements.txt
