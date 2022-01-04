4bda20e917c70931155f95f6428869e5cacd1003
Aaron Vaz <vazaaron08@gmail.com>
2022-01-04 08:33:06 +0000

    add find alias for macos

--- zsh-plugins/init.zsh
+++ zsh-plugins/init.zsh
@@ -2,7 +2,7 @@
 
 export ZSH_PLUGINS_DIR=$DOTFILES/zsh-plugins/.plugins
 
-for plugin in $DOTFILES/zsh-plugins/.plugins/**/*.plugin.zsh; do
+for plugin in $ZSH_PLUGINS_DIR/**/*.plugin.zsh; do
   echo "Initialising $(echo $plugin | cut -d '/' -f 4-)"
   source $plugin
 done
