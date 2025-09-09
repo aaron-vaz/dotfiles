3c6d935ca9b0ef597e596487288387b96b7e5190
Aaron Vaz <vazaaron08@gmail.com>
2025-09-09 21:07:05 +0100

    Add git aliases and update VSCode settings for GitLens integration

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,3 @@
 alias glp="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gpp="git pull --prune"
+alias gf="git fetch --progress --prune"
